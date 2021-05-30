7363df8af23b4c4c26fb4e536e87b36568ce472a
funny-recipe-0681 <ekow.bentsi-enchill@yale.edu>
2021-05-30 04:45:25 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,7 @@
 
 def meal_choice(veg1, veg2, protein = 'tofu')
   puts "What a nutritious meal!"
-  message = "A plate of #{protein} with #{veg1} and #{veg2}"
-  puts message
-  message
+  meal = "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts meal
+  return meal
 end
-
